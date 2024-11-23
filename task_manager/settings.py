f8054bbdb86a880cc83db510ce86d1878d9236b0
aniketlokhande420 <aniketlokhande420@gmail.com>
2024-11-24 00:46:11 +0530

    initial commit for render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "task-manager-1tvf.onrender.com"]
+ALLOWED_HOSTS = ["task-manager-1tvf.onrender.com"]
 
 
 # Application definition
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
